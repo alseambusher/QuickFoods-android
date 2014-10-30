56a6717f69cf94d68789dcb7c0f346d12951a7d1
Suresh Alse <alseambusher@gmail.com>
2014-10-31 02:03:50 +0530

    swipe list view added

--- src/com/intuit/quickfoods/PlaceholderTakeOrder.java
+++ src/com/intuit/quickfoods/PlaceholderTakeOrder.java
@@ -9,8 +9,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PlaceholderTakeOrder extends PlaceholderBase {
 	
@@ -40,7 +44,72 @@ public class PlaceholderTakeOrder extends PlaceholderBase {
                 view.findViewById(R.id.take_order_add_item);
         take_order_add_item.setAdapter(adapter);
 
-		return view;
+        final ArrayAdapter mAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                new ArrayList<String>(Arrays.asList(Data.food_items)));
+
+        ListView listView = new ListView(getActivity());
+        listView.setAdapter(mAdapter);
+        // Create a ListView-specific touch listener. ListViews are given special treatment because
+        // by default they handle touches for their list items... i.e. they're in charge of drawing
+        // the pressed state (the list selector), handling list item clicks, etc.
+        SwipeDismissListViewTouchListener touchListener =
+                new SwipeDismissListViewTouchListener(
+                        listView,
+                        new SwipeDismissListViewTouchListener.DismissCallbacks() {
+                            @Override
+                            public boolean canDismiss(int position) {
+                                return true;
+                            }
+
+                            @Override
+                            public void onDismiss(ListView listView, int[] reverseSortedPositions) {
+                                for (int position : reverseSortedPositions) {
+                                    mAdapter.remove(mAdapter.getItem(position));
+                                }
+                                mAdapter.notifyDataSetChanged();
+                            }
+                        });
+        listView.setOnTouchListener(touchListener);
+        // Setting this scroll listener is required to ensure that during ListView scrolling,
+        // we don't look for swipes.
+        listView.setOnScrollListener(touchListener.makeScrollListener());
+
+        // Set up normal ViewGroup example
+        final ViewGroup dismissableContainer = (ViewGroup) view.findViewById(R.id.dismissable_container);
+        for (int i = 0; i < Data.food_items.length; i++) {
+            final Button dismissableButton = new Button(getActivity());
+            dismissableButton.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            dismissableButton.setText(Data.food_items[i]);
+            dismissableButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getActivity(),
+                            "Clicked " + ((Button) view).getText(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+            // Create a generic swipe-to-dismiss touch listener.
+            dismissableButton.setOnTouchListener(new SwipeDismissTouchListener(
+                    dismissableButton,
+                    null,
+                    new SwipeDismissTouchListener.DismissCallbacks() {
+                        @Override
+                        public boolean canDismiss(Object token) {
+                            return true;
+                        }
+
+                        @Override
+                        public void onDismiss(View view, Object token) {
+                            dismissableContainer.removeView(dismissableButton);
+                        }
+                    }));
+            dismissableContainer.addView(dismissableButton);
+        }
+
+        return view;
 	}
 	
 	public void refreshItemList(){
